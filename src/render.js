9740d15a3b1eba233d87cf64321eb2e2f9626121
Ivan <oldrum75@gmai.com>
2023-06-10 12:53:25 +0200

    fix: replacing try catch with than catch

--- src/render.js
+++ src/render.js
@@ -15,46 +15,33 @@ const render = (rssData) => {
       const innerDiv = document.createElement('div');
       innerDiv.classList.add('bg-secondary', 'rounded', 'text-info', 'p-4', 'mb-4');
 
-      div.appendChild(innerDiv);
-      containerList.appendChild(div);
-
       // Выполняем парсинг RSS
       axios.get(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(link)}&disableCache=true`)
         .then((response) => {
           if (response.status === 200) {
-            try {
-              const data = parser(response);
-              const title = data.querySelector('title');
-              const description = data.querySelector('description');
+            const data = parser(response);
+            const title = data.querySelector('title');
+            const description = data.querySelector('description');
 
-              const pParsedTitle = document.createElement('p');
-              pParsedTitle.textContent = `Title: ${title.textContent}`;
+            const pParsedTitle = document.createElement('p');
+            pParsedTitle.textContent = `Title: ${title.textContent}`;
 
-              const pParsedDescription = document.createElement('p');
-              pParsedDescription.textContent = `Description: ${description.textContent}`;
+            const pParsedDescription = document.createElement('p');
+            pParsedDescription.textContent = `Description: ${description.textContent}`;
 
-              innerDiv.appendChild(pParsedTitle);
-              innerDiv.appendChild(pParsedDescription);
-            } catch (error) {
-              console.error(error);
-              showErrorMessage(innerDiv);
-            }
+            div.appendChild(innerDiv);
+            containerList.appendChild(div);
+            innerDiv.appendChild(pParsedTitle);
+            innerDiv.appendChild(pParsedDescription);
           }
         })
         .catch((error) => {
           console.error(error);
-          showErrorMessage(innerDiv);
+          alert('An error occurred while parsing the RSS data');
+          // showErrorMessage(innerDiv);
         });
     }
   });
-};
-
-const showErrorMessage = (parentElement) => {
-  const errorMessage = document.createElement('div');
-  errorMessage.textContent = 'An error occurred while parsing the RSS data';
-  // Добавьте стили или классы для отображения сообщения об ошибке
-
-  parentElement.appendChild(errorMessage);
 };
 
 export default render;
