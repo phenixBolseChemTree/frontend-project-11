9db4fc1a30b2c1d21fd92ced4771f2dafaa8252a
Ivan <oldrum75@gmai.com>
2023-06-10 03:58:35 +0200

    feat: axios get response

--- src/formController.js
+++ src/formController.js
@@ -1,9 +1,12 @@
 import i18next from 'i18next';
 import onChange from 'on-change';
 import * as yup from 'yup';
+import axios from 'axios';
+import parser from './parserRSS.js';
 import render from './render.js';
 
 const formController = () => {
+  let id = 0;
   const rssLinks = [];
   const rssSchema = yup.string().url().required().matches(/\.rss$/);
   const form = document.querySelector('form');
@@ -22,17 +25,31 @@ const formController = () => {
       });
   });
 
-  // Создаем прокси для отслеживания изменений в массиве rssLinks
   const watchedRssLinks = onChange(rssLinks, (path, value, previousValue) => {
     if (path.includes('length')) {
       // Обрабатываем изменения длины массива rssLinks (push, pop, splice и т.д.)
       return;
     }
-    // Обрабатываем другие изменения в массиве rssLinks
+
     console.log('rssLinks изменился:');
     console.log('path:', path); // Измененный путь
     console.log('value:', value); // Новое значение
     console.log('previousValue:', previousValue); // Предыдущее значение
+
+    // axios.get(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(value)}&disableCache=true`)
+    //   .then((response) => {
+    //     if (response.status === 200) {
+    //       const data = parser(response);
+    //       const title = data.querySelector('title');
+    //       const description = data.querySelector('description');
+    //       console.log('Parsed data:');
+    //       console.log('Title:', title.textContent);
+    //       console.log('Description:', description.textContent);
+    //     }
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
   });
 
   form.addEventListener('submit', (event) => {
@@ -45,7 +62,8 @@ const formController = () => {
             if (!watchedRssLinks.includes(resolve)) {
               input.classList.remove('is-invalid');
               watchedRssLinks.push(resolve);
-              render(resolve);
+              id += 1;
+              render([{ id, link: resolve }]);
               console.log(i18next.t('successfulScenario')); // Успешный сценарий
               event.target.reset();
               input.focus();
--- src/render.js
+++ src/render.js
@@ -1,22 +1,60 @@
-const render = (link) => {
+import axios from 'axios';
+import parser from './parserRSS.js';
+
+const render = (rssData) => {
   const containerList = document.querySelector('.container-list');
-  const existingElement = Array.from(containerList.children).find((element) => element.querySelector('p').textContent === `volid: ${link}`);
 
-  if (!existingElement) {
-    const div = document.createElement('div');
-    div.classList.add('container', 'w-100');
+  rssData.forEach(({ id, link }) => {
+    const existingElement = Array.from(containerList.children).find((element) => element.querySelector('p').textContent === `volid: ${link}`);
 
-    const innerDiv = document.createElement('div');
-    innerDiv.classList.add('bg-secondary', 'rounded', 'text-info', 'p-4', 'mb-4');
+    if (!existingElement) {
+      const div = document.createElement('div');
+      div.id = id;
+      div.classList.add('container', 'w-100');
 
-    const p = document.createElement('p');
-    p.classList.add('h2');
-    p.textContent = link;
+      const innerDiv = document.createElement('div');
+      innerDiv.classList.add('bg-secondary', 'rounded', 'text-info', 'p-4', 'mb-4');
 
-    innerDiv.appendChild(p);
-    div.appendChild(innerDiv);
-    containerList.appendChild(div);
-  }
+      div.appendChild(innerDiv);
+      containerList.appendChild(div);
+
+      // Выполняем парсинг RSS
+      axios.get(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(link)}&disableCache=true`)
+        .then((response) => {
+          if (response.status === 200) {
+            try {
+              const data = parser(response);
+              const title = data.querySelector('title');
+              const description = data.querySelector('description');
+
+              const pParsedTitle = document.createElement('p');
+              pParsedTitle.textContent = `Title: ${title.textContent}`;
+
+              const pParsedDescription = document.createElement('p');
+              pParsedDescription.textContent = `Description: ${description.textContent}`;
+
+              innerDiv.appendChild(pParsedTitle);
+              innerDiv.appendChild(pParsedDescription);
+            } catch (error) {
+              console.error(error);
+              showErrorMessage(innerDiv);
+            }
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+          showErrorMessage(innerDiv);
+        });
+    }
+  });
+};
+
+const showErrorMessage = (parentElement) => {
+  const errorMessage = document.createElement('div');
+  errorMessage.textContent = 'An error occurred while parsing the RSS data';
+  // Добавьте стили или классы для отображения сообщения об ошибке
+
+  parentElement.appendChild(errorMessage);
 };
 
 export default render;
