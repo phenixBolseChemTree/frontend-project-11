a03e67108a98ddf824f6bf8dfb9ad0bbb6136832
Ivan <oldrum75@gmai.com>
2023-08-18 15:41:52 +0200

    fix: try/catch > Error handling

--- src/parse.js
+++ src/parse.js
@@ -1,21 +1,23 @@
 const parser = (dataStr) => {
-  try {
-    const dataObj = JSON.parse(dataStr);
-    const domParser = new DOMParser();
-    const data = domParser.parseFromString(dataObj.data.contents, 'application/xml');
-    const title = data.querySelector('title').textContent;
-    const description = data.querySelector('description').textContent;
-    const postsReverse = [...data.querySelectorAll('item')].reverse().map((nodeItem) => ({
-      title: nodeItem.querySelector('title').textContent,
-      description: nodeItem.querySelector('description').textContent,
-      link: nodeItem.querySelector('link').textContent,
-      pubDate: nodeItem.querySelector('pubDate').textContent,
-    }));
-    const posts = postsReverse.reverse();
-    return { title, description, posts };
-  } catch (e) {
+  const dataObj = JSON.parse(dataStr);
+  const domParser = new DOMParser();
+  const data = domParser.parseFromString(dataObj.data.contents, 'application/xml');
+  const errorNode = data.querySelector('parsererror');
+
+  if (errorNode) {
     throw new Error('invalidRSS');
   }
+
+  const title = data.querySelector('title').textContent;
+  const description = data.querySelector('description').textContent;
+  const postsReverse = [...data.querySelectorAll('item')].reverse().map((nodeItem) => ({
+    title: nodeItem.querySelector('title').textContent,
+    description: nodeItem.querySelector('description').textContent,
+    link: nodeItem.querySelector('link').textContent,
+    pubDate: nodeItem.querySelector('pubDate').textContent,
+  }));
+  const posts = postsReverse.reverse();
+  return { title, description, posts };
 };
 
 export default parser;
